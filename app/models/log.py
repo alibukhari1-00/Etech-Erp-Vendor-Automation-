894fa6e52d134a53f0a6bf1fa3001d68bc53166a
gellahi <gellahi.dev@gmail.com>
2026-03-23 01:39:09 +0500

    develop[db constraint]: Fix the Log with Projects FK Constraint

--- app/models/log.py
+++ app/models/log.py
@@ -7,7 +7,7 @@ class Log(Base):
     __tablename__ = "logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    project_id = Column(Integer, ForeignKey("projects.id"), nullable=False)
+    project_id = Column(Integer, ForeignKey("projects.id", ondelete="CASCADE"), nullable=False)
     status = Column(String(255), nullable=False)
 
     project = relationship("Project")
